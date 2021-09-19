4e75c95f847e367c508493faadff8add187ded42
Jonathan Sheng <js165@rice.edu>
2021-09-19 03:27:13 -0500

    testing excel

--- backend/delegator/algo.py
+++ backend/delegator/algo.py
@@ -2,15 +2,66 @@ import time
 from read_excel import Data
 import json
 
+class Facility():
+    def __init__(self, dict):
+        self.facility = dict["Facility"]
+        self.latitude = dict["Latitude"]
+        self.longitude = dict["Longitude"]
+        self.maxOccupacy = dict["Maximum Occupacy"]
+        self.currentOccupacy = 0
+    def getFacility(self):
+        return self.facility
+    def getLat(self):
+        return self.latitude
+    def getLong(self):
+        return self.longitude
+    def getMaxOccupacy(self):
+        return self.maxOccupacy
+    def getCurrOccupacy(self):
+        return self.currentOccupacy
+        
+class Worker():
+    def __init__(self, dict):
+        self.name = dict["Name"]
+        self.certifications = dict["Certifications"]
+        self.shifts = dict["Shifts"]
+        self.location = dict["Location"]
+        self.hasJob = dict["hasJob"]
+    def getName(self):
+        return self.name
+    def getCert(self):
+        return self.certifications
+    def getShifts(self):
+        return self.shifts
+    def getLocation(self):
+        return self.location
+    def getHasJob(self):
+        return self.hasJob
+class Log():
+    def __init__(self, data):
+        self.workID = data["Work Order #"]
+        self.facility = data["Facility"]
+        self.equipment = data["Equipment Type"]
+        self.equipID = data["Equipment ID"]
+        self.priority = data["Priority(1-5)"]
+        self.timeNeeded = data["Time to Complete"]
+        self.timeSubmission = data["Submission Timestamp"]
+        
 def checkCertifications(technician, orderList):
-    return [x for x in orderList if x.type in technician.certifications]
+    res = []
+    for order in orderList:
+        if order.equipment in technician.certifications:
+            res.append(order)
+    return res
+    
 
 def checkOccupancy(orderList, facilitiesList):
     notOccupied = []
+    print(orderList, facilitiesList)
     for order in orderList:
         for fac in facilitiesList:
-            if fac == order.facility:
-                if fac.currentOccupancy < fac.maxOccupancy:
+            if fac.facility == order.facility:
+                if fac.currentOccupacy < fac.maxOccupacy:
                     notOccupied.append(order)
     return notOccupied
         
@@ -22,7 +73,7 @@ def assignOrderToTech(technician, orderList, facilitiesList):
     """
     # filters only the orders that technician has certification for 
     filter1 = checkCertifications(technician, orderList); 
-
+    print("passes filter 1")
     # and then filters only the non fully occupied facilities
     finalList = checkOccupancy(filter1, facilitiesList)
 
@@ -39,15 +90,30 @@ def assignOrderToTech(technician, orderList, facilitiesList):
         if currentLocation == order.facility:
             return tuple(technician, order)
 
-    return tuple(technician, finalList[0])
+    return tuple(technician.name, finalList[0])
 
 def testExcel():
     x = Data()
     workerData = x.getWorkerData()
-    jsonWorker = json.dumps(workerData)
+    facData = x.getFacilityData()
+    lstWorkerObj = []
+    lstFacObj = []
+    lstLogObj = []
     logData = x.getLogData()
     logData = sorted(logData, key=lambda k: (k['Priority(1-5)']), reverse = True)
-    jsonLog = json.dumps(logData)
-    print(type(jsonWorker), jsonLog)
+    for worker in workerData:
+        lstWorkerObj.append(Worker(worker))
+    for facility in facData:
+        lstFacObj.append(Facility(facility))
+    for log in logData:
+        lstLogObj.append(Log(log))
+    for worker in lstWorkerObj:
+        print(assignOrderToTech(worker, lstLogObj, lstFacObj))
+    
+    #jsonWorker = json.dumps(workerData)
+    #logData = x.getLogData()
+    #logData = sorted(logData, key=lambda k: (k['Priority(1-5)']), reverse = True)
+    #jsonLog = json.dumps(logData)
+    #print(type(jsonWorker), jsonLog)
 
 testExcel()
